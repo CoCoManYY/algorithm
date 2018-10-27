9fb22df0354673a6991aaefa909026f0893ba3c1
CoCoManYY <1344483030@qq.com>
2018-10-28 03:04:59 +0800

    半数集问题

---
+++ 半数集问题/HalfSet.js
@@ -0,0 +1,16 @@
+// 1、递归表达式
+// 2、准确表达程序出口
+// 3、根据表达式准确写出函数返回值
+function halfSet(n){
+    if(n==1){
+        return 1;
+    }
+    let count=1;
+    for(let i=1;i<=n/2;i++){
+        console.log('循环'+i);
+        count+=await halfSet(i);
+    }
+    return count;
+}
+halfSet(8);
+// console.log(halfSet(8));
