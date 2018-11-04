756ae7c06b6cc4fe894392525bb20c9d176037d0
CoCoManYY <1344483030@qq.com>
2018-11-04 10:59:33 +0800

    删数问题

---
+++ 删数问题(贪心问题)/delk.js
@@ -0,0 +1,25 @@
+// 问题描述
+// 给定 n 位正整数 a，去掉其中任意 k≤n 个数字后，剩下的数字按原次序排列组成一个 新的正整数。对于给定的 n 位正整数 a 和正整数 k，设计一个算法找出剩下数字组成的新数 最小的删数方案。
+// 对于给定的正整数 a，编程计算删去 k 个数字后得到的最小数。
+// 数据输入： 
+// 第 1 行是 1 个正整数 a。第 2 行是正整数 k。
+function delk(){
+   if(k>=m) return;
+   while(k>0){
+       for(var i=0;i<m-1&&a[i]<a[i+1];i++);
+       a.splice(i,1);
+       k--;
+   }
+   while(m>1&&a[0]=='0')
+        a.splice(0,1);
+}
+function main(){
+    a=178543;//整数a
+    k=4;//删除的数字个数
+    a=a.toString();
+    a=a.split('');
+    m=a.length;//数字长度
+    delk();
+    console.log(a);
+}
+main();
